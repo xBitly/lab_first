cc5409cf917c5394694f8a2766b42b20b85e9ebe
Andrey Avdeev <64530588+xBitly@users.noreply.github.com>
2022-10-20 11:56:15 +0300

    Correct ex6, version 0.6

--- ex6.kt
+++ ex6.kt
@@ -1,18 +1,79 @@
-import java.util.*
+import java.lang.Exception
+import java.lang.IndexOutOfBoundsException
+
+class DoublyLinkedList<T: Comparable<T>>{
+    class DoublyLinkedNode<T: Comparable<T>> (var value: Comparable<T>) {
+        var prev: DoublyLinkedNode <T>? = null
+        var next: DoublyLinkedNode <T>? = null
+    }
+
+    private var end: DoublyLinkedNode<T>? = null
+    private var start: DoublyLinkedNode<T>? = null
+
+    fun add(value: Comparable<T>){
+        val node = DoublyLinkedNode(value)
+        node.next = null
+        node.prev = end
+        if (end == null) {
+            end = node
+            start = end
+        } else end!!.next = node
+        end = node
+    }
+
+    fun set(index: Int, value: Comparable<T>){
+        var obj = start
+        val node = DoublyLinkedNode(value)
+        for (i in 0 until index){
+            obj = obj?.next
+        }
+        if (obj != null) {
+            node.next = obj.next
+            node.prev = obj.prev
+            obj.prev?.next = node
+            obj.next?.prev = node
+        } else {
+            throw IndexOutOfBoundsException("Index: $index")
+        }
+        if(index == 0){
+            start = node
+        }
+    }
+
+    fun contains(value: Comparable<T>): Boolean{
+        var obj = start
+        while (obj != null){
+            if (obj.value == value) return true
+            obj = obj.next
+        }
+        return false
+    }
+
+    fun convertToString(): String{
+        var str = ""
+        var obj = start
+        while (true){
+            str += obj?.value.toString() + " "
+            obj = obj?.next
+            if (obj == null) break
+        }
+        return str
+    }
+}
 
 fun main() {
-    val list = LinkedList<Int>()
-    list.add(5)
-    list.add(2)
-    list.add(1)
-    list.add(4)
-    println("Список после добавления элементов: $list")
-    list.set(2, 9)
-    println("Список после изменения второго(по индексу) элемента: $list")
-    val cont = list.contains(2)
-    println("Проверка содержится ли элемент(2) в списке: $cont")
-    list.remove(9)
-    println("Список после удаления 9 из списка: $list")
-    val index = list.indexOf(4)
-    println("Индекс элемента(4) в списке: $index")
+    val list = DoublyLinkedList<Int>()
+    list.add(3)
+    list.add(6)
+    list.add(9)
+    list.add(11)
+    print("Список после добавления элементов: ")
+    println(list.convertToString())
+    list.set(2, 7)
+    print("Список после изменения второго(по индексу) элемента: ")
+    println(list.convertToString())
+
+    val cont = list.contains(3)
+    print("Проверка содержится ли элемент(3) в списке: ")
+    println(cont)
 }
